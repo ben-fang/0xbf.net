fcb8a86e9f185599a736a3f4006edfa77524671b
BenFangR <fyz.ben@outlook.com>
2023-10-17 16:52:36 +0800

    2023.10.17

--- config/email.ts
+++ config/email.ts
@@ -1,7 +1,7 @@
 export const emailConfig = {
-  from: 'hi@cali.so',
+  from: 'fyz.ben@icloud.com',
   baseUrl:
     process.env.VERCEL_ENV === 'production'
-      ? `https://cali.so`
+      ? `https://0xbf.net`
       : 'http://localhost:3000',
 }
