70281e43b770feffabd63c4a7c9c5ea69d5e2e9b
BenFangR <fyz.ben@outlook.com>
2023-10-20 20:23:21 +0800

    Optimize personal infomation

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,11 +14,11 @@ import { seo } from '~/lib/seo'
 export const metadata: Metadata = {
   metadataBase: seo.url,
   title: {
-    template: '%s | Cali Castle',
+    template: '%s | Ben Fang',
     default: seo.title,
   },
   description: seo.description,
-  keywords: 'Cali,Cali Castle,郭晓楠,佐玩,创始人,CEO,开发者,设计师,细节控,创新',
+  keywords: 'Ben,Ben Fang,创始人,开发者,创新',
   themeColor: [
     { media: '(prefers-color-scheme: dark)', color: '#000212' },
     { media: '(prefers-color-scheme: light)', color: '#fafafa' },
@@ -38,13 +38,13 @@ export const metadata: Metadata = {
   openGraph: {
     title: {
       default: seo.title,
-      template: '%s | Cali Castle',
+      template: '%s | Ben Fang',
     },
     description: seo.description,
-    siteName: 'Cali Castle',
+    siteName: 'Ben Fang',
     locale: 'zh_CN',
     type: 'website',
-    url: 'https://cali.so',
+    url: 'https://0xbf.net',
   },
   twitter: {
     site: '@thecalicastle',
